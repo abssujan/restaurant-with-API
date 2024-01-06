a6e09ee1f22fef1bd417b6bf0875d564077c085a
abssujan <abdussabursujan@gmail.com>
2024-01-06 21:02:41 +0600

    set up details

--- js/app.js
+++ js/app.js
@@ -8,12 +8,13 @@ const restaurantThumnil = (seacrh) => {
 
 const loadRestaurantDetails = (meal) => {
     const restaurantDetails = document.getElementById('restaurant-details');
+    console.log(meal)
     restaurantDetails.innerHTML = ``
     meal.forEach( meals => {
         const createDiv = document.createElement('div');
         createDiv.classList.add('col');
         createDiv.innerHTML = `
-      <div class="card">
+      <div onclick = "loadMealDeatails(${meals.idMeal})" class="card">
         <img src=" ${meals.strMealThumb} " class="card-img-top" alt="...">
         <div class="card-body">
         <h5 class="card-title">${meals.strMeal}</h5>
@@ -30,5 +31,27 @@ const searchWithName = () => {
     const searchValue = searchInput.value;
     restaurantThumnil(searchValue)
     searchInput.value = ``;
+}
+
+const loadMealDeatails = (idMeal) => {
+ const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+ fetch(url)
+ .then(res => res.json())
+ .then(data => displayMealDetails(data.meals[0]))
+}
+const displayMealDetails = (meal) => {
+  const mealDetailsContainer = document.getElementById('meals-details');
+  mealDetailsContainer.innerHTML = ``;
+  const containerDiv = document.createElement('div');
+  containerDiv.classList.add('card');
+  containerDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+         <h5 class="card-title">${meal.strMeal}</h5>
+         <p class="card-text">${(meal.strInstructions).slice(0,200)}</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  `;
+  mealDetailsContainer.appendChild(containerDiv)
 }
 restaurantThumnil('')
